f9c3f9c076bb026b4655da97adb25fb31856d25b
Matthew Smrzlick <msmrzlic@uncc.edu>
2016-02-26 00:43:36 +0000

    Done with section 4.6.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,17 +6,20 @@ class MoviesController < ApplicationController
   
   
   def show
-    debugger
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.html.haml by default
   end
+  
+  def new
+    # default: render 'new' template
+  end
 
 private 
 
-def movie_params
-  params.require(:movie).permit(:title, :rating, :description, :release_date)
-end
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
 
 end
 
